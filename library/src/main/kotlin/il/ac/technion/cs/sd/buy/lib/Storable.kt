f4b71ddd9d6bbb944fc2f93358538e75e80597a0
o.mahmud <O.mahmud@campus.technion.ac.il>
2025-01-24 21:50:09 +0200

    required storable interface funcs ... you can update what you , just tell me to consider that in my code , if something is hard to implement also tell me so i try to change implementation in my code and cancel it ...

--- library/src/main/kotlin/il/ac/technion/cs/sd/buy/lib/Storable.kt
+++ library/src/main/kotlin/il/ac/technion/cs/sd/buy/lib/Storable.kt
@@ -4,9 +4,46 @@ interface Storable{
 
 }
 
-interface StorableReviews : Storable {
-    fun getReviewersAvgScore() : Map<String , String>//v
-    fun getReviewersReviews() : Map<String , List<String>>//
-    fun getBooksAvgScore() : Map<String , String>// v
-    fun getBooksReviewerScore() : Map<String ,  List<Pair<String, String>>> // v
+interface StorableProducts : Storable {
+    /**
+    ** returns a map that maps productIDs to the quantity of  purchased items of them- do not include canceled orders
+     */
+    fun getAllProducts():Map<String,String>
+
+
+    /**
+     ** returns a map that maps productIDs to how much they cost (how much one costs...)
+     * consider that their can be more than definition for a product and so cost can be changed
+     */
+    fun getAllPrices():Map<String,String>
+
+
+}
+
+
+    interface  StorableUsers : Storable {
+        /**
+         * returns a map that maps user-ids to pair of first::status and second::list of their orders , possibly some of them canceled or modified
+         * status can be canceled , modified and nmc ; nmc for not modified neither canceled-
+         * if its hard to give the status here also its ok ... it will be in OrdersIDsWithStatus
+         */
+       fun getUserIDsWithOrders():Map<String,Pair<String,List<String>>>
+
+    }
+interface StorableOrders:Storable {
+    /**
+     * *return map of all valid order ids , possibly canceled also ..
+     * with their status : canceled , modified or nmc
+     */
+    fun getOrdersIDsWithStatus():Map<String,String>
+    /**
+     * returns a map that maps order-ids to a pair of : the product and the modifications ...
+     * if the order was cancelled we can add -1 in the end
+     *
+     * check piazza @54 and @50 , they will give us some answers
+     */
+    fun getOrderIDsWithProducts():Map<String,Pair<String,List<String>>>
+
+
+    
 }
