44cfb65444a695f35db1d21c01c5262513a2d525
o.mahmud <O.mahmud@campus.technion.ac.il>
2025-01-25 16:41:19 +0200

    merged all the interfaces in one for simpling things

--- library/src/main/kotlin/il/ac/technion/cs/sd/buy/lib/Storable.kt
+++ library/src/main/kotlin/il/ac/technion/cs/sd/buy/lib/Storable.kt
@@ -4,7 +4,7 @@ interface Storable{
 
 }
 
-interface StorableProducts : Storable {
+interface StorableMarket : Storable {
     /**
     ** returns a map that maps productIDs to the quantity of  purchased items of them- do not include canceled orders
      */
@@ -16,21 +16,12 @@ interface StorableProducts : Storable {
      * consider that their can be more than definition for a product and so cost can be changed
      */
     fun getAllPrices():Map<String,String>
-
-
-}
-
-
-    interface  StorableUsers : Storable {
-        /**
-         * returns a map that maps user-ids to pair of first::status and second::list of their orders , possibly some of them canceled or modified
-         * status can be canceled , modified and nmc ; nmc for not modified neither canceled-
-         * if its hard to give the status here also its ok ... it will be in OrdersIDsWithStatus
-         */
-       fun getUserIDsWithOrders():Map<String,Pair<String,List<String>>>
-
-    }
-interface StorableOrders:Storable {
+    /**
+     * returns a map that maps user-ids to pair of first::status and second::list of their orders , possibly some of them canceled or modified
+     * status can be canceled , modified and nmc ; nmc for not modified neither canceled-
+     * if its hard to give the status here also its ok ... it will be in OrdersIDsWithStatus
+     */
+    fun getUserIDsWithOrders():Map<String,Pair<String,List<String>>>
     /**
      * *return map of all valid order ids , possibly canceled also ..
      * with their status : canceled , modified or nmc
@@ -45,5 +36,5 @@ interface StorableOrders:Storable {
     fun getOrderIDsWithProducts():Map<String,Pair<String,List<String>>>
 
 
-    
 }
+
